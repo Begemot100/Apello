701789e70403eb40667f10b116ba9573514d4d8e
Begemot100 <76606767+Begemot100@users.noreply.github.com>
2021-07-19 11:37:12 +0300

    Add files via upload

---
+++ _final.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from tkinter import Tk, font
+
+from PIL.ImageFont import truetype
+
+font.root = Tk()
+# print(font.families())
+
+def image_signature(directory):
+    for file in os.listdir(directory): # loop through the folder
+        if file.endswith(".jpg"):
+            name = os.path.splitext(file)[0].replace("-", ' ').title()  # format the file name
+            image = Image.open(directory + "/" + file)
+            image.load() # loading the library
+            font = truetype('Avenir',size=15)
+            draw_text = ImageDraw.Draw(image)
+            draw_text.text((image.size[0] - 100,image.size[1] - 50), name,fill='white',font=font)
+            image.save(directory + "/" + file) # save the result
+            image.show()
+
+
+print('This script signs images from the file name')
+directory = input('Enter the path to the folder with files:')
+image_signature(directory)
+print('Ready!')
